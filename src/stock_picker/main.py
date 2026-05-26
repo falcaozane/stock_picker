a6c4752e697b63a5223d522f2b03e0248633901e
falcaozane <falcaozane@gmail.com>
2026-05-26 15:33:40 +0530

    stock picker using crewai

---
+++ src/stock_picker/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import warnings
+import os
+from datetime import datetime
+
+from stock_picker.crew import StockPicker
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+def run():
+    """
+    Run the research crew.
+    """
+
+    # Ask user for company name
+    sector = input("Enter the sector name to select stocks from: ").strip()
+
+    if not sector:
+        print("Sector name cannot be empty. Please try again.")
+        return
+
+    inputs = {
+        'sector': sector,
+        "current_date": str(datetime.now())
+    }
+
+    # Create and run the crew
+    result = StockPicker().crew().kickoff(inputs=inputs)
+
+    # Print the result
+    print("\n\n=== FINAL DECISION ===\n\n")
+    print(result.raw)
+
+
+if __name__ == "__main__":
+    run()
